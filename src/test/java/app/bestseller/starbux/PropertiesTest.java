c42f4b10102f51a5e5a0cfda2e1bb62bd05f5213
mahbodkh <ebrahim.khosravani@latoken.com>
2021-08-16 23:05:55 +0430

    finish ReportControllerTests.

--- src/test/java/app/bestseller/starbux/PropertiesTest.java
+++ src/test/java/app/bestseller/starbux/PropertiesTest.java
@@ -1,9 +1,28 @@
 package app.bestseller.starbux;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
 /**
  * Created by Ebrahim Kh.
  */
 
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@EnableConfigurationProperties(value = Properties.class)
 public class PropertiesTest {
+    private @Autowired
+    Properties properties;
+
+    @Test
+    @Transactional
+    public void givenProperties_whenBindingPropertiesFileTest() {
+
+    }
 }
